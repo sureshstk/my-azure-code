fd5aaccd3400d81c092ab604681bd485e748e75c
sureshstk <sureshstk4me@gmail.com>
2024-06-24 16:33:19 -0500

    Create factorial.php

---
+++ factorial.php
@@ -0,0 +1,14 @@
+<?php
+$number1 = 10;
+$number2 = 5;
+
+$sum = $number1 + $number2;
+$difference = $number1 - $number2;
+$product = $number1 * $number2;
+$quotient = $number1 / $number2;
+
+echo "Sum: $sum<br>";
+echo "Difference: $difference<br>";
+echo "Product: $product<br>";
+echo "Quotient: $quotient<br>";
+?>
